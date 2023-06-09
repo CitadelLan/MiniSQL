329aacbdb82d363b8315277492be03c908459325
CitadelLan <3173459373@qq.com>
2023-06-09 16:29:28 +0800

    Updated 0609

--- src/executor/insert_executor.cpp
+++ src/executor/insert_executor.cpp
@@ -29,21 +29,20 @@ bool InsertExecutor::Next([[maybe_unused]] Row *row, RowId *rid) {
   tableHeap = tableInfo->GetTableHeap();
 
   /* 1. 获取values_executor中的元组 */
-  child_executor_->Next(row, nullptr);
+  if(child_executor_->Next(row, nullptr)) {
+    /* 2. 插入元组 */
+    if (tableHeap->InsertTuple(*row, nullptr)) {
+      /* 2.1. 检查是否包含索引，如果有，更新索引 */
+      Schema *schema = tableInfo->GetSchema();
+      const std::vector<Column *> columns = schema->GetColumns(0);
+      IndexInfo *index = nullptr;
 
-  /* 2. 插入元组 */
-  if(tableHeap->InsertTuple(*row, nullptr)) {
-    /* 2.1. 检查是否包含索引，如果有，更新索引 */
-    Schema *schema = tableInfo->GetSchema();
-    const std::vector<Column *> columns = schema->GetColumns(0);
-    IndexInfo *index = nullptr;
+      for (auto it : columns)
+        if (catalog->GetIndex(tableName, it->GetName(), index) == dberr_t::DB_SUCCESS)
+          index->GetIndex()->InsertEntry(*row, *rid, nullptr);
 
-    for(auto it : columns)
-      if(catalog->GetIndex(tableName, it->GetName(), index) == dberr_t::DB_SUCCESS)
-        index->GetIndex()->InsertEntry(*row, *rid, nullptr);
-
-    return true;
+      return true;
+    }
   }
-
   return false;
 }
